cc93b93ef8982b7f716cb2b0cba51cd22fa43f93
Alex Soto <apsoto@gmail.com>
2010-12-03 15:26:45 -0800

    added pagination of search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,8 +6,10 @@ class SearchController < ApplicationController
     if params[:searchTerms].blank?
       @results = []
     else
+      @page = params[:startPage] || 1
+      per_page = params[:perPage] || 20
       @api = MetachannelsApi.new(METACHANNELS_APIKEY, METACHANNELS_ENDPOINT)
-      @results = @api.search("searchTerms" => params[:searchTerms], "order" => "keyword")
+      @results = @api.search("searchTerms" => params[:searchTerms], "order" => "keyword", "perPage" => per_page, "startPage" => @page)
     end
   end
 
--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'metachannels-api-client'
   config.gem 'libxml-ruby', :lib => 'xml/libxml'
+  config.gem 'will_paginate'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
