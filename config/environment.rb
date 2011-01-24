27ad5765cd5405791540563838c6ad00fb19b6b2
teresa <teresa@tkdevvm.tacitknowledge.com>
2011-01-24 13:36:41 -0800

    remove sqlite3 dependency since it is not needed

--- config/environment.rb
+++ config/environment.rb
@@ -15,7 +15,7 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem "sqlite3-ruby", :lib => "sqlite3"
+  #config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem 'metachannels-api-client'
   config.gem 'libxml-ruby', :lib => 'xml/libxml'
   config.gem 'will_paginate'
